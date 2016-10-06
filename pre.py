6db7bd11998a23bef4c1532f63289df1418841ee
brianLeeson <brianeleeson@gmail.com>
2016-10-06 14:28:40 -0700

    arrow test

--- pre.py
+++ pre.py
@@ -6,75 +6,74 @@ import arrow
 base = arrow.now()
 
 def process(raw):
-    """
-    Line by line processing of syllabus file.  Each line that needs
-    processing is preceded by 'head: ' for some string 'head'.  Lines
-    may be continued if they don't contain ':'.  If # is the first
-    non-blank character on a line, it is a comment ad skipped. 
-    """
-    field = None
-    entry = { }
-    cooked = [ ] 
-    for line in raw:
-        line = line.strip()
-        if len(line) == 0 or line[0]=="#" : #skips blanks and comments
-            continue
-        parts = line.split(':')
-        if len(parts) == 1 and field:
-            entry[field] = entry[field] + line + " "
-            continue
-        if len(parts) == 2: 
-            field = parts[0]
-            content = parts[1]
-        else:
-            raise ValueError("Trouble with line: '{}'\n".format(line) + 
-                "Split into |{}|".format("|".join(parts)))
+	"""
+	Line by line processing of syllabus file.  Each line that needs
+	processing is preceded by 'head: ' for some string 'head'.  Lines
+	may be continued if they don't contain ':'.  If # is the first
+	non-blank character on a line, it is a comment ad skipped. 
+	"""
+	field = None
+	entry = { }
+	cooked = [ ] 
+	for line in raw:
+		line = line.strip()
+		if len(line) == 0 or line[0]=="#" : #skips blanks and comments
+			continue
+		parts = line.split(':')
+		if len(parts) == 1 and field:
+			entry[field] = entry[field] + line + " "
+			continue
+		if len(parts) == 2: 
+			field = parts[0]
+			content = parts[1]
+		else:
+			raise ValueError("Trouble with line: '{}'\n".format(line) + 
+				"Split into |{}|".format("|".join(parts)))
 
-        if field == "begin":
-            try:
-                base = arrow.get(content, "MM/DD/YYYY")
-                print("Base date {}".format(base.isoformat()))
-            except:
-                raise ValueError("Unable to parse date {}".format(content))
+		if field == "begin":
+			try:
+				base = arrow.get(content, "MM/DD/YYYY")
+				print("Base date {}".format(base.isoformat()))
+			except:
+				raise ValueError("Unable to parse date {}".format(content))
 
-        elif field == "week":
-            if entry:
-                cooked.append(entry)
-                entry = { }
-            entry['topic'] = ""
-            entry['project'] = ""
-            entry['week'] = content
-            #The following line 
-            week_date = (base.replace(weeks =+ int(entry['week']) - 1))
-            entry['date'] = week_date.format('MM/DD/YYYY')
-            
-            # I don't know if this goes here.
-            current_time = arrow.utcnow()
-            print(current_time)
-            print(week_date)
-            print(current_time > week_date)
-            
-            # want to say
-            #if current_time > week_date:
-            #    highlight week
+		elif field == "week":
+			if entry:
+				cooked.append(entry)
+				entry = { }
+			entry['topic'] = ""
+			entry['project'] = ""
+			entry['week'] = content
+			#The following line 
+			week_start = (base.replace(weeks =+ int(entry['week']) - 1))
+			entry['date'] = week_start.format('MM/DD/YYYY')
+			
+			# I don't know if this goes here.
+			today = arrow.utcnow()
+			week_end = week_start.replace(weeks =+ 1)
+			#print(current_week)
+			#print(week_date)
+			#print(current_week > week_date)
+			if (week_start < today) and (today < week_end):			
+				entry['highlight'] = True
+			
+		elif field == 'topic' or field == 'project':
+			entry[field] = content
 
-        elif field == 'topic' or field == 'project':
-            entry[field] = content
+		else:
+			raise ValueError("Syntax error in line: {}".format(line))
 
-        else:
-            raise ValueError("Syntax error in line: {}".format(line))
+	if entry:
+		cooked.append(entry)
 
-    if entry:
-        cooked.append(entry)
-
-    return cooked
+	return cooked
 
 
 def main():
-    f = open("data/schedule.txt")
-    parsed = process(f)
-    f.close()
-    print(parsed)
+	f = open("data/schedule.txt")
+	parsed = process(f)
+	f.close()
+	print(parsed)
 
 if __name__ == "__main__":
-    main()
+	main()
